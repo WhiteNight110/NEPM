5e3daa1efeb81bdf99bb0432c106de1c9d5a5743
wyb <1368387047@qq.com>
2024-06-17 14:06:55 +0800

    JWT && GlobalCorsConfig

--- src/main/java/com/neusoft/nepm/controller/AqiController.java
+++ src/main/java/com/neusoft/nepm/controller/AqiController.java
@@ -7,12 +7,15 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
+@CrossOrigin
+//@CrossOrigin(origins = "*")
 @Controller
 @Api(tags = "AqiController", description = "AQI查询")
 @RequestMapping("/aqi")
--- src/main/java/com/neusoft/nepm/service/impl/AdminsServiceImpl.java
+++ src/main/java/com/neusoft/nepm/service/impl/AdminsServiceImpl.java
@@ -1,12 +1,19 @@
 package com.neusoft.nepm.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.neusoft.nepm.common.utils.JwtUtil;
 import com.neusoft.nepm.po.Admins;
 import com.neusoft.nepm.mapper.AdminsMapper;
 import com.neusoft.nepm.service.AdminsService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashMap;
 
 /**
  * <p>
@@ -25,23 +32,54 @@ public class AdminsServiceImpl extends ServiceImpl<AdminsMapper, Admins> impleme
 
     @Override
     public String adminsLogin(Admins admins) {
-        Admins admin = adminsMapper.selectOne(new QueryWrapper<Admins>().eq("admin_code", admins.getAdminCode()).eq("password", admins.getPassword()));
+
+        Admins admin = adminsMapper.selectOne(new QueryWrapper<Admins>().eq("admin_code", admins.getAdminCode()));
+
+//        System.out.println("===============================");
 
         if(admin == null){
             return "FAILED";
         }
-        return "SUCCESS";
+
+        // 判断密码是否正确
+        String encodedPassword = admin.getPassword();
+//        System.out.println(encodedPassword);
+//        System.out.println("===============================");
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        // 参数：(明文，密文)
+        boolean result = passwordEncoder.matches(admins.getPassword(), encodedPassword);
+        if(result){
+//            System.out.println("==============");
+//            System.out.println(admin.getAdminId());
+//            System.out.println("================");
+
+            // 生成令牌
+            HashMap<String, Object> map = new HashMap<>(2);
+            map.put("userId", admin.getAdminId());
+            String token = JwtUtil.generateToken(map);
+
+            return token;
+//            return "SUCCESS";
+        }
+        return "FAILED";
 
     }
 
     @Override
     public String adminRegister(Admins admins) {
         Admins admin = adminsMapper.selectOne(new QueryWrapper<Admins>().eq("admin_code", admins.getAdminCode()));
-        if(admin != null){
+        if(admin == null){
+            // 密码加密
+            PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+            String encodedPassword = passwordEncoder.encode(admins.getPassword());
+            admins.setPassword(encodedPassword);
+
             adminsMapper.insert(admins);
             return "SUCCESS";
         }
-        else return "FAILED";
+        else {
+            return "FAILED";
+        }
     }
 
     @Override
