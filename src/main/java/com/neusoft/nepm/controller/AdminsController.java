74e626fd460d1a0579b633ca874fe2c94bfb463d
wyb <1368387047@qq.com>
2024-06-21 00:07:20 +0800

    登录验证码模块

--- src/main/java/com/neusoft/nepm/controller/AdminsController.java
+++ src/main/java/com/neusoft/nepm/controller/AdminsController.java
@@ -1,21 +1,14 @@
 package com.neusoft.nepm.controller;
 
-import cn.hutool.captcha.CaptchaUtil;
-import cn.hutool.captcha.LineCaptcha;
 import com.neusoft.nepm.common.api.CommonResult;
+import com.neusoft.nepm.common.api.ValidateCodeVo;
 import com.neusoft.nepm.config.CaptchaProperties;
 import com.neusoft.nepm.po.Admins;
 import com.neusoft.nepm.service.AdminsService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.io.IOException;
-import java.util.Date;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -28,6 +21,8 @@ public class AdminsController {
 
     @Autowired
     private CaptchaProperties captchaProp;
+
+
 
     //验证码生效时间限制
     private static final long VALID_MILLIS_TIME = 60 * 1000;
@@ -62,41 +57,15 @@ public class AdminsController {
 
     @ApiOperation("获取验证码")
     @GetMapping("/getCaptcha")
-    public void getCaptcha(HttpServletResponse response, HttpSession session) {
-        //定义图形验证码的长和宽(配置默认值)
-        LineCaptcha lineCaptcha = CaptchaUtil.createLineCaptcha(captchaProp.getWidth(), captchaProp.getHeight());
-        //细节问题，不影响程序
-        //设置返回类型
-        response.setContentType("image/jpeg");
-        //静止缓存
-        response.setHeader("Progma", "No-cache");
-        try {
-            //图形验证码写出，可以写出到文件，也可以写出到流
-            lineCaptcha.write(response.getOutputStream());
-            //同时将验证码内容和当前时间戳存储到Session中
-            //此处Session的键可以配置成常量
-            session.setAttribute(captchaProp.getSession().getKey(), lineCaptcha.getCode());
-            session.setAttribute(captchaProp.getSession().getDate(), new Date());
-            //关流
-            response.getOutputStream().close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    @ResponseBody
+    public ValidateCodeVo getCaptcha(){
+        return adminsService.generateValidateCode();
     }
 
     @ApiOperation("校验验证码")
-    @GetMapping("/checkCaptcha")
-    public boolean checkCaptcha(String captcha, HttpSession session) {
-        //保证传过来的参数是合法的
-        if (StringUtils.hasLength(captcha)) {
-            //根据配置的默认session信息获取key和date
-            String key = (String) session.getAttribute(captchaProp.getSession().getKey());
-            Date date = (Date) session.getAttribute(captchaProp.getSession().getDate());
-            //1.验证码正确(不区分大小写)  2.验证码还未失效
-            return key.equalsIgnoreCase(captcha)
-                    && System.currentTimeMillis() - date.getTime() < VALID_MILLIS_TIME;
-        }
-        return false;
+    @PostMapping("/checkCaptcha")
+    public boolean checkCaptcha(@RequestParam String captcha, @RequestParam String codeKey) {
+        return adminsService.verifyValidateCode(captcha, codeKey);
     }
 
 
--- src/main/java/com/neusoft/nepm/service/impl/AdminsServiceImpl.java
+++ src/main/java/com/neusoft/nepm/service/impl/AdminsServiceImpl.java
@@ -1,19 +1,26 @@
 package com.neusoft.nepm.service.impl;
 
+import cn.hutool.captcha.CaptchaUtil;
+import cn.hutool.captcha.LineCaptcha;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.neusoft.nepm.common.api.ValidateCodeVo;
 import com.neusoft.nepm.common.utils.JwtUtil;
 import com.neusoft.nepm.po.Admins;
 import com.neusoft.nepm.mapper.AdminsMapper;
 import com.neusoft.nepm.service.AdminsService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 
 import java.util.HashMap;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 /**
  * <p>
@@ -28,6 +35,9 @@ public class AdminsServiceImpl extends ServiceImpl<AdminsMapper, Admins> impleme
 
     @Autowired
     private AdminsMapper adminsMapper;
+
+    @Autowired
+    private RedisTemplate<String , String> redisTemplate;
 
 
     @Override
@@ -80,6 +90,45 @@ public class AdminsServiceImpl extends ServiceImpl<AdminsMapper, Admins> impleme
         else {
             return "FAILED";
         }
+    }
+
+    @Override
+    public ValidateCodeVo generateValidateCode() {
+        //定义图形验证码的长和宽(配置默认值)
+        LineCaptcha lineCaptcha = CaptchaUtil.createLineCaptcha(150, 48, 4, 20);
+
+        String codeValue = lineCaptcha.getCode();
+        String imageBase64 = lineCaptcha.getImageBase64();
+
+        String codeKey = UUID.randomUUID().toString().replace("-", "");
+
+        // 将验证码存储到Redis中
+        redisTemplate.opsForValue().set("user:login:validatecode:" + codeKey , codeValue , 5 , TimeUnit.MINUTES);
+
+        // 构建响应结果数据
+        ValidateCodeVo validateCodeVo = new ValidateCodeVo() ;
+        validateCodeVo.setCodeKey(codeKey);
+        validateCodeVo.setCodeValue("data:image/png;base64," + imageBase64);
+        // 返回数据
+        return validateCodeVo;
+    }
+
+    @Override
+    public boolean verifyValidateCode(String captcha, String codeKey) {
+        //保证传过来的参数是合法的
+        if (StringUtils.hasLength(captcha)) {
+
+            String key = redisTemplate.opsForValue().get(codeKey).toString();
+            System.out.println("==============");
+            System.out.println(key);
+
+            if(key.equalsIgnoreCase(captcha)){
+                // 登录成功，删除Redis中的验证码
+                redisTemplate.delete(codeKey);
+                return true;
+            };
+        }
+        return false;
     }
 
     @Override
